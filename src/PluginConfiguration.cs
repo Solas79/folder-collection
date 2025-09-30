201f3e1fc37f872f88532d129905115ddbaf87ad
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-09-30 18:13:47 +0200

    Update PluginConfiguration.cs

--- src/PluginConfiguration.cs
+++ src/PluginConfiguration.cs
@@ -1,16 +1,17 @@
-using MediaBrowser.Model.Plugins;
 using System.Collections.Generic;
+using MediaBrowser.Model.Plugins;
 
-namespace Jellyfin.Plugin.FolderCollections.GUI;
-
-public class PluginConfiguration : BasePluginConfiguration
+namespace Jellyfin.Plugin.FolderCollections.GUI
 {
-public bool IncludeMovies { get; set; } = true;
-public bool IncludeSeries { get; set; } = true;
-public List<string> LibraryPathPrefixes { get; set; } = new();
-public List<string> IgnorePatterns { get; set; } = new(); // regex
-public int MinimumItemsPerFolder { get; set; } = 2;
-public bool UseBasenameForCollection { get; set; } = true;
-public string NamePrefix { get; set; } = string.Empty;
-public string NameSuffix { get; set; } = string.Empty;
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        public bool IncludeMovies { get; set; } = true;
+        public bool IncludeSeries { get; set; } = true;
+        public List<string> LibraryPathPrefixes { get; set; } = new();
+        public List<string> IgnorePatterns { get; set; } = new(); // regex
+        public int MinimumItemsPerFolder { get; set; } = 2;
+        public bool UseBasenameForCollection { get; set; } = true;
+        public string NamePrefix { get; set; } = string.Empty;
+        public string NameSuffix { get; set; } = string.Empty;
+    }
 }
