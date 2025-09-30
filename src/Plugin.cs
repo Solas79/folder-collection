a388b7dd7f3d689e726ae51fbcd74be251b63f09
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-09-30 17:36:00 +0200

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -5,34 +5,39 @@ using MediaBrowser.Model.Serialization;
 using System;
 using System.IO;
 
-namespace Jellyfin.Plugin.FolderCollections.GUI;
-
-public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+namespace Jellyfin.Plugin.FolderCollections.GUI
 {
-public override string Name => "Folder Collections";
-public override Guid Id { get; } = Guid.Parse("4bb2a3d2-b8c6-4b3f-bf2c-d1a3e4e9b7a1");
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        public static Plugin? Instance { get; private set; }
 
-public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
-: base(applicationPaths, xmlSerializer)
-{
+        public override string Name => "Folder Collections";
+        public override Guid Id { get; } = Guid.Parse("4bb2a3d2-b8c6-4b3f-bf2c-d1a3e4e9b7a1");
+
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public override string Description => "Group library items by parent folder into BoxSets (configurable).";
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = "config",
+                    EmbeddedResourcePath = GetType().Namespace + ".Web.Configuration.config.html"
+                },
+                new PluginPageInfo
+                {
+                    Name = "config.js",
+                    EmbeddedResourcePath = GetType().Namespace + ".Web.Configuration.config.js"
+                }
+            };
+        }
+    }
 }
 
-public override string Description => "Group library items by parent folder into BoxSets (configurable).";
-
-public IEnumerable<PluginPageInfo> GetPages()
-{
-return new[]
-{
-new PluginPageInfo
-{
-Name = "config",
-EmbeddedResourcePath = GetType().Namespace + ".Web.Configuration.config.html"
-},
-new PluginPageInfo
-{
-Name = "config.js",
-EmbeddedResourcePath = GetType().Namespace + ".Web.Configuration.config.js"
-}
-};
-}
-}
