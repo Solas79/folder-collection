0deefc23904583aec065017a34e965b27007aedd
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-01 22:21:27 +0200

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -10,7 +10,7 @@ namespace FolderCollections;
 public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
     public override string Name => "Folder Collections";
-    public override Guid Id => new("YOUR-GUID-HERE-1234-5678-90AB-ABCDEF012345"); // fest lassen, nicht ändern
+    public override Guid Id => new("9f4f2c47-b3c5-4b13-9b1f-1c9a5c3b8d6a"); // fest lassen, nicht ändern
 
     public Plugin(IApplicationPaths appPaths, IXmlSerializer xml)
         : base(appPaths, xml) { }
