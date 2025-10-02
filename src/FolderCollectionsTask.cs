426cfd1c4ca6134a3e844be682fc0865a366d273
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-03 00:51:41 +0200

    Update FolderCollectionsTask.cs

--- src/FolderCollectionsTask.cs
+++ src/FolderCollectionsTask.cs
@@ -3,7 +3,7 @@
 @@
  using System;
  using System.Collections.Generic;
-+using System.IO;
+ using System.IO;
  using System.Threading;
  using System.Threading.Tasks;
  using MediaBrowser.Model.Tasks;
