a5d94cd05d6336306ed896eeb4f904d857c30a07
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-02 12:28:05 +0200

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,4 +1,3 @@
-// src/FolderCollections/Plugin.cs
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
@@ -8,20 +7,24 @@ namespace FolderCollections;
 
 public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
-    public override string Name => "Folder Collections";
-    public override Guid Id => new("9f4f2c47-b3c5-4b13-9b1f-1c9a5c3b8d6a"); // fest lassen, nicht ändern
+    public static Plugin? Instance { get; private set; }
 
-    public Plugin(IApplicationPaths appPaths, IXmlSerializer xml)
-        : base(appPaths, xml) { }
+    public override string Name => "Folder Collections";
+    public override Guid Id => new("9f4f2c47-b3c5-4b13-9b1f-1c9a5c3b8d6a");
+
+    public Plugin(IApplicationPaths appPaths, IXmlSerializer xml) : base(appPaths, xml)
+    {
+        Instance = this; // wichtig für Zugriff aus anderen Klassen
+    }
 
     public IEnumerable<PluginPageInfo> GetPages() => new[]
     {
         new PluginPageInfo
         {
             Name = "folderCollectionsConfigPage",
-            EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.html",
-            // Jellyfin erkennt daran, dass es eine volle Plugin-Konfig-Seite ist:
-            PageType = PluginPageType.PluginConfiguration
+            EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.config.html"
+            // KEIN PageType hier – verursacht den CS0117/CS0103 Fehler
         }
     };
 }
+
