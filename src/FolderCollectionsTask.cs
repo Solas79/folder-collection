dd5b6505d9a7b23277019d26cceeb04f92df3800
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-02 13:15:59 +0200

    Update FolderCollectionsTask.cs

--- src/FolderCollectionsTask.cs
+++ src/FolderCollectionsTask.cs
@@ -5,7 +5,7 @@ using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Reflection;
-
+using FolderCollections;
 using Jellyfin.Data.Enums;
 using MediaBrowser.Controller.Collections;
 using MediaBrowser.Controller.Entities;
