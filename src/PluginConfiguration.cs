0305986556b06e5e5f9a76c029f1c246beb2845a
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-02 12:31:41 +0200

    Update PluginConfiguration.cs

--- src/PluginConfiguration.cs
+++ src/PluginConfiguration.cs
@@ -1,7 +1,3 @@
-using MediaBrowser.Model.Plugins;
-
-namespace FolderCollections;
-
 public class PluginConfiguration : BasePluginConfiguration
 {
     public bool IncludeMovies { get; set; } = false;
@@ -10,6 +6,7 @@ public class PluginConfiguration : BasePluginConfiguration
     public string? Prefix { get; set; }
     public string? Suffix { get; set; }
     public int ScanHour { get; set; } = 4;
+    public int ScanMinute { get; set; } = 0;  // <-- ergänzt
     public string[] PathPrefixes { get; set; } = Array.Empty<string>();
     public string[] IgnorePatterns { get; set; } = Array.Empty<string>();
 }
