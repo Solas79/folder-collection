02d1d1c13015a299f02c48afaf06ef4ced8035a5
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-02 16:53:26 +0200

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -2,7 +2,7 @@ using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
-using MediaBrowser.Controller.Plugins:
+using MediaBrowser.Controller.Plugins;
 
 namespace FolderCollections;
 
