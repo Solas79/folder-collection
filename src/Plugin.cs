bc74239bf5156afc69fd8168ba1d7609ea1aa4bd
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-02 19:46:49 +0200

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -8,19 +8,6 @@ using MediaBrowser.Model.Serialization;
 
 namespace FolderCollections
 {
-    public class PluginConfiguration : BasePluginConfiguration
-    {
-        public bool IncludeMovies { get; set; } = true;
-        public bool IncludeSeries { get; set; } = true;
-        public int MinItems { get; set; } = 2;
-        public string Prefix { get; set; } = "";
-        public string Suffix { get; set; } = "";
-        public int ScanHour { get; set; } = 4;
-        public int ScanMinute { get; set; } = 0;
-        public string[] PathPrefixes { get; set; } = Array.Empty<string>();
-        public string[] IgnorePatterns { get; set; } = Array.Empty<string>();
-    }
-
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
         public static Plugin? Instance { get; private set; }
@@ -38,7 +25,6 @@ namespace FolderCollections
         {
             new PluginPageInfo
             {
-                // Name MUSS "config" heißen, sonst kein Zahnrad
                 Name = "config",
                 EmbeddedResourcePath = "FolderCollections.Web.Configuration.config.html"
             },
