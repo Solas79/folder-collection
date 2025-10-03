5c54301305b78812c6155324ede8ac5e9da63a60
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-03 22:46:40 +0200

    Update PluginConfiguration.cs

--- src/PluginConfiguration.cs
+++ src/PluginConfiguration.cs
@@ -1,21 +1,13 @@
-using System.Collections.Generic;
 using MediaBrowser.Model.Plugins;
 
 namespace FolderCollections
 {
-    public sealed class PluginConfiguration : BasePluginConfiguration
+    public class PluginConfiguration : BasePluginConfiguration
     {
-        public List<string> IncludeFolders { get; set; } = new();
-        public List<string> ExcludeFolders { get; set; } = new();
+        public string? RootPath { get; set; }
+        public bool Recursive { get; set; } = true;
 
-        public string? CollectionNamePrefix { get; set; } = "";
-        public string? CollectionNameSuffix { get; set; } = "";
-
-        public int  MinItemsPerFolder { get; set; } = 2;
-
-        public bool   EnableDailyScan  { get; set; } = false;
-        public string DailyScanTime    { get; set; } = "03:00";
-        public int    DailyScanHour    { get; set; } = 3;   // Back-Compat
-        public int    DailyScanMinute  { get; set; } = 0;   // Back-Compat
+        // Platz für weitere Optionen
+        public string? CollectionNamePrefix { get; set; } = "FC:";
     }
 }
