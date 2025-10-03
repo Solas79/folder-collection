c32b03b4c44df88185f722b1cbe9df36542ae9d3
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-03 20:02:24 +0200

    Update ConfigUIControllers.cs

--- src/Controllers/ConfigUIControllers.cs
+++ src/Controllers/ConfigUIControllers.cs
@@ -1,23 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
-using FolderCollections;
+using FolderCollections; // für Embedded
 
-namespace FolderCollections.Web;
-
-[ApiController]
-[Route("FolderCollections")]
-public class ConfigUiController : ControllerBase
+namespace FolderCollections.Web
 {
-    [HttpGet("ui")]
-    public IActionResult Ui()
+    [ApiController]
+    [Route("FolderCollections")]
+    public class ConfigUiController : ControllerBase
     {
-        var html = Embedded.ReadAllText("FolderCollections.Web.ui.config.html");
-        return Content(html, "text/html; charset=utf-8");
-    }
+        // 1) Diagnose-Endpunkt: http(s)://<server>:8096/FolderCollections/ping  ->  200 "pong"
+        [HttpGet("ping")]
+        public IActionResult Ping() => Content("pong", "text/plain");
 
-    [HttpGet("ui/config.js")]
-    public IActionResult UiJs()
-    {
-        var js = Embedded.ReadAllText("FolderCollections.Web.ui.fc-config.js");
-        return Content(js, "application/javascript; charset=utf-8");
+        // 2) Standalone HTML: http(s)://<server>:8096/FolderCollections/ui
+        [HttpGet("ui")]
+        public IActionResult Ui()
+        {
+            var html = Embedded.ReadAllText("FolderCollections.Web.ui.config.html");
+            return Content(html, "text/html; charset=utf-8");
+        }
+
+        // 3) Standalone JS: http(s)://<server>:8096/FolderCollections/ui/config.js
+        [HttpGet("ui/config.js")]
+        public IActionResult UiJs()
+        {
+            var js = Embedded.ReadAllText("FolderCollections.Web.ui.fc-config.js");
+            return Content(js, "application/javascript; charset=utf-8");
+        }
     }
 }
