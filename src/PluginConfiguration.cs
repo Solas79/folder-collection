fa1d1f768694059bdec8b00e5713219d9a159c0e
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-03 06:24:22 +0200

    Update PluginConfiguration.cs

--- src/PluginConfiguration.cs
+++ src/PluginConfiguration.cs
@@ -1,33 +1,22 @@
 using System;
-using System.Collections.Generic;
-using MediaBrowser.Common.Configuration;
-using MediaBrowser.Common.Plugins;
-using MediaBrowser.Controller.Plugins;
-using MediaBrowser.Model.Plugins;
-using MediaBrowser.Model.Serialization;
 
 namespace FolderCollections
 {
-    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    public class PluginConfiguration
     {
-        public static Plugin? Instance { get; private set; }
+        public bool IncludeMovies { get; set; } = true;
+        public bool IncludeSeries { get; set; } = false;
+        public int  MinItems { get; set; } = 2;
 
-        public override string Name => "Folder Collections";
-        public override Guid Id => new Guid("9f4f2c47-b3c5-4b13-9b1f-1c9a5c3b8d6a");
+        public string? Prefix { get; set; } = "";
+        public string? Suffix { get; set; } = "";
 
-        public Plugin(IApplicationPaths appPaths, IXmlSerializer xml)
-            : base(appPaths, xml)
-        {
-            Instance = this;
-        }
+        public int ScanHour   { get; set; } = 4;
+        public int ScanMinute { get; set; } = 0;
 
-        public IEnumerable<PluginPageInfo> GetPages() => new[]
-        {
-            new PluginPageInfo
-            {
-                Name = "config",
-                EmbeddedResourcePath = "FolderCollections.Web.Configuration.config.html"
-            }
-        };
+        public string[] PathPrefixes   { get; set; } = Array.Empty<string>();
+        public string[] IgnorePatterns { get; set; } = Array.Empty<string>();
+
+        public bool UseBasenameAsCollectionName { get; set; } = true;
     }
 }
