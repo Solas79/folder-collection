ad15c0c076595d826a3ab127a048a4104a69687a
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-03 18:35:22 +0200

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,33 +1,21 @@
-using System;
+// src/PluginPages.cs
 using System.Collections.Generic;
-using MediaBrowser.Common.Configuration;
-using MediaBrowser.Common.Plugins;
-using MediaBrowser.Controller.Plugins;
 using MediaBrowser.Model.Plugins;
-using MediaBrowser.Model.Serialization;
 
-namespace FolderCollections
+namespace FolderCollections.Web
 {
-    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    // Achtung: IHasWebPages (nicht IPluginConfigurationPage)
+    public sealed class PluginPages : IHasWebPages
     {
-        public static Plugin? Instance { get; private set; }
-
-        public override string Name => "Folder Collections";
-        public override Guid Id => new Guid("9f4f2c47-b3c5-4b13-9b1f-1c9a5c3b8d6a");
-
-        public Plugin(IApplicationPaths appPaths, IXmlSerializer xml)
-            : base(appPaths, xml)
+        public IEnumerable<PluginPageInfo> GetPages()
         {
-            Instance = this;
-        }
-
-        public IEnumerable<PluginPageInfo> GetPages() => new[]
-        {
-            new PluginPageInfo
+            // Launcher-Seite, die auf die Standalone-UI verweist
+            yield return new PluginPageInfo
             {
                 Name = "config",
-                EmbeddedResourcePath = "FolderCollections.Web.Configuration.config.html"
-            }
-        };
+                EmbeddedResourcePath = "FolderCollections.Web.redirect.launch.html",
+                EnableInMainMenu = true
+            };
+        }
     }
 }
