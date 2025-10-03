3e53d097b9204b79fd1774d1bd8994569cc73122
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-03 17:55:19 +0200

    Update ConfigUIControllers.cs

--- src/Controllers/ConfigUIControllers.cs
+++ src/Controllers/ConfigUIControllers.cs
@@ -1,1 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
 
+namespace FolderCollections.Web;
+
+[ApiController]
+[Route("FolderCollections")]
+public class ConfigUiController : ControllerBase
+{
+    [HttpGet("ui")]
+    public IActionResult Ui()
+    {
+        var html = Embedded.ReadAllText("FolderCollections.Web.ui.config.html");
+        return Content(html, "text/html; charset=utf-8");
+    }
+
+    [HttpGet("ui/config.js")]
+    public IActionResult UiJs()
+    {
+        var js = Embedded.ReadAllText("FolderCollections.Web.ui.fc-config.js");
+        return Content(js, "application/javascript; charset=utf-8");
+    }
+}
