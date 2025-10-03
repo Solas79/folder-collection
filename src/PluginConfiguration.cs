c96206b89210727988b91e60bd127b7748a784a0
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-03 05:03:02 +0200

    Update PluginConfiguration.cs

--- src/PluginConfiguration.cs
+++ src/PluginConfiguration.cs
@@ -1,25 +1,33 @@
 using System;
-using MediaBrowser.Model.Plugins; // <- WICHTIG: Hier kommt BasePluginConfiguration her
+using System.Collections.Generic;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
 
 namespace FolderCollections
 {
-    public class PluginConfiguration : BasePluginConfiguration
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        public bool IncludeMovies { get; set; } = true;
-        public bool IncludeSeries { get; set; } = true;
+        public static Plugin? Instance { get; private set; }
 
-        public int MinItems { get; set; } = 2;
+        public override string Name => "Folder Collections";
+        public override Guid Id => new Guid("9f4f2c47-b3c5-4b13-9b1f-1c9a5c3b8d6a");
 
-        public string? Prefix { get; set; }
-        public string? Suffix { get; set; }
+        public Plugin(IApplicationPaths appPaths, IXmlSerializer xml)
+            : base(appPaths, xml)
+        {
+            Instance = this;
+        }
 
-        // Checkbox "Basename als Sammlungsname"
-        public bool UseBasenameAsCollectionName { get; set; } = true;
-
-        public int ScanHour   { get; set; } = 4;
-        public int ScanMinute { get; set; } = 0;
-
-        public string[] PathPrefixes   { get; set; } = Array.Empty<string>();
-        public string[] IgnorePatterns { get; set; } = Array.Empty<string>();
+        public IEnumerable<PluginPageInfo> GetPages() => new[]
+        {
+            new PluginPageInfo
+            {
+                Name = "config",
+                EmbeddedResourcePath = "FolderCollections.Web.Configuration.config.html"
+            }
+        };
     }
 }
