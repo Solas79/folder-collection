28097a5f81e9fbdcf69c5e010d93c209cdc30c45
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-03 17:56:43 +0200

    Update PluginPages.cs

--- src/PluginPages.cs
+++ src/PluginPages.cs
@@ -1,1 +1,18 @@
+using MediaBrowser.Model.Plugins;
 
+namespace FolderCollections.Web
+{
+    public sealed class PluginPages : IPluginConfigurationPage
+    {
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            // kleiner Launcher, der /FolderCollections/ui im Top-Level öffnet
+            yield return new PluginPageInfo
+            {
+                Name = "config",
+                EmbeddedResourcePath = "FolderCollections.Web.redirect.launch.html",
+                EnableInMainMenu = true
+            };
+        }
+    }
+}
