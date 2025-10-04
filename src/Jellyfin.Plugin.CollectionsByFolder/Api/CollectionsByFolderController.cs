813373734ba315413e36ac0911420bd018a28ceb
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-04 23:43:30 +0200

    Update CollectionsByFolderController.cs

--- src/Jellyfin.Plugin.CollectionsByFolder/Api/CollectionsByFolderController.cs
+++ src/Jellyfin.Plugin.CollectionsByFolder/Api/CollectionsByFolderController.cs
@@ -1,17 +1,31 @@
-using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Jellyfin.Plugin.CollectionsByFolder.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Jellyfin.Plugin.CollectionsByFolder.Api
 {
     [ApiController]
-    [Route("CollectionsByFolder/Debug")]
-    public class DebugController : ControllerBase
+    [Route("CollectionsByFolder")]
+    public class CollectionsByFolderController : ControllerBase
     {
-        [HttpGet("Resources")]
-        public IActionResult Resources()
+        [HttpPost("ScanNow")]
+        public async Task<IActionResult> ScanNow(CancellationToken ct)
         {
-            var names = typeof(Plugin).Assembly.GetManifestResourceNames().OrderBy(n => n);
-            return Ok(names);
+            var builder = new CollectionBuilder();
+            var applier = new CollectionsApplier();
+
+            var candidates  = await builder.BuildCollectionsAsync(ct);
+            var applyResult = await applier.ApplyAsync(candidates, ct);
+
+            return Ok(new
+            {
+                started    = true,
+                candidates = candidates.Count,
+                created    = applyResult.Created,
+                updated    = applyResult.Updated,
+                skipped    = applyResult.Skipped
+            });
         }
     }
 }
