09bd9810ffe8b9f1aadb74dcecb17eea214c3052
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-04 17:01:19 +0200

    Update Plugin.cs

--- src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
+++ src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using MediaBrowser.Common.Configuration;  // <- IApplicationPaths ist hier
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
@@ -6,34 +9,37 @@ namespace Jellyfin.Plugin.CollectionsByFolder
 {
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
+        public static Plugin Instance { get; private set; } = null!;
+
         public override string Name => "CollectionsByFolder";
         public override string Description => "Erstellt automatisch Sammlungen nach Ordnernamen.";
 
-        public static Plugin Instance { get; private set; }
-
-        // 10.10: BasePlugin benötigt nur noch IXmlSerializer
-        public Plugin(IXmlSerializer xmlSerializer) : base(xmlSerializer)
+        // Diese API-Version erwartet IApplicationPaths + IXmlSerializer
+        public Plugin(IApplicationPaths appPaths, IXmlSerializer xmlSerializer)
+            : base(appPaths, xmlSerializer)
         {
             Instance = this;
         }
+
+        public override Guid Id => Guid.Parse("f58f3a40-6a8a-48e8-9b3a-9d7f0b6a3a41");
 
         public IEnumerable<PluginPageInfo> GetPages()
         {
             var ns = GetType().Namespace;
             return new[]
             {
+                // HTML-Seite
                 new PluginPageInfo
                 {
                     Name = "collectionsbyfolder",
                     EmbeddedResourcePath = ns + ".Configuration.index.html",
-                    Type = PluginPageType.Html,    // iframe-Seite
                     EnableInMainMenu = true
                 },
+                // zugehöriges JS
                 new PluginPageInfo
                 {
                     Name = "collectionsbyfolderjs",
-                    EmbeddedResourcePath = ns + ".Configuration.index.js",
-                    Type = PluginPageType.Script
+                    EmbeddedResourcePath = ns + ".Configuration.index.js"
                 }
             };
         }
