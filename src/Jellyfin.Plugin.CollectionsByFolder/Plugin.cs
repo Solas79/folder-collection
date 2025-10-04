2d1c2ebd53e55c63005f1023dc5e5df5442f8831
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-04 19:21:04 +0200

    Update Plugin.cs

--- src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
+++ src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
@@ -1,6 +1,5 @@
-using System;
 using System.Collections.Generic;
-using MediaBrowser.Common.Configuration;   // IApplicationPaths
+using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
@@ -9,35 +8,30 @@ namespace Jellyfin.Plugin.CollectionsByFolder
 {
     public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        public static Plugin Instance { get; private set; } = null!;
-
         public override string Name => "CollectionsByFolder";
         public override string Description => "Erstellt automatisch Sammlungen nach Ordnernamen.";
-        public override Guid Id => Guid.Parse("f58f3a40-6a8a-48e8-9b3a-9d7f0b6a3a41");
+        public override System.Guid Id => System.Guid.Parse("f58f3a40-6a8a-48e8-9b3a-9d7f0b6a3a41");
 
         public Plugin(IApplicationPaths appPaths, IXmlSerializer xmlSerializer)
-            : base(appPaths, xmlSerializer)
+            : base(appPaths, xmlSerializer) { }
+
+        public IEnumerable<PluginPageInfo> GetPages()
         {
-            Instance = this;
+            var ns = GetType().Namespace; // <- nimmt den echten Namespace der Assembly
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = "collectionsbyfolder",
+                    EmbeddedResourcePath = ns + ".Configuration.index.html",
+                    EnableInMainMenu = true
+                },
+                new PluginPageInfo
+                {
+                    Name = "collectionsbyfolderjs",
+                    EmbeddedResourcePath = ns + ".Configuration.index.js"
+                }
+            };
         }
-
-       public IEnumerable<PluginPageInfo> GetPages()
-       {
-           return new[]
-           {
-               new PluginPageInfo
-               {
-                   Name = "collectionsbyfolder",
-                   EmbeddedResourcePath = "Jellyfin.Plugin.CollectionsByFolder.Configuration.index.html",
-                   EnableInMainMenu = true
-               },
-               new PluginPageInfo
-               {
-                   Name = "collectionsbyfolderjs",
-                   EmbeddedResourcePath = "Jellyfin.Plugin.CollectionsByFolder.Configuration.index.js"
-               }
-           };
-       }
-
     }
 }
