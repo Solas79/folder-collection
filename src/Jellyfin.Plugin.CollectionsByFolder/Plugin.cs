8e0bee06232bdd1ccc9b2614ee1c79ec5b879066
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-04 17:35:22 +0200

    Update Plugin.cs

--- src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
+++ src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using MediaBrowser.Common.Configuration;  // <- IApplicationPaths ist hier
+using MediaBrowser.Common.Configuration;   // IApplicationPaths
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
@@ -13,32 +13,28 @@ namespace Jellyfin.Plugin.CollectionsByFolder
 
         public override string Name => "CollectionsByFolder";
         public override string Description => "Erstellt automatisch Sammlungen nach Ordnernamen.";
+        public override Guid Id => Guid.Parse("f58f3a40-6a8a-48e8-9b3a-9d7f0b6a3a41");
 
-        // Diese API-Version erwartet IApplicationPaths + IXmlSerializer
         public Plugin(IApplicationPaths appPaths, IXmlSerializer xmlSerializer)
             : base(appPaths, xmlSerializer)
         {
             Instance = this;
         }
 
-        public override Guid Id => Guid.Parse("f58f3a40-6a8a-48e8-9b3a-9d7f0b6a3a41");
-
         public IEnumerable<PluginPageInfo> GetPages()
         {
-            var ns = GetType().Namespace;
+            var ns = GetType().Namespace; // "Jellyfin.Plugin.CollectionsByFolder"
             return new[]
             {
-                // HTML-Seite
                 new PluginPageInfo
                 {
-                    Name = "collectionsbyfolder",
+                    Name = "collectionsbyfolder",                           // URL: ...?name=collectionsbyfolder
                     EmbeddedResourcePath = ns + ".Configuration.index.html",
                     EnableInMainMenu = true
                 },
-                // zugehöriges JS
                 new PluginPageInfo
                 {
-                    Name = "collectionsbyfolderjs",
+                    Name = "collectionsbyfolderjs",                         // MUSS mit <script src="..."> matchen
                     EmbeddedResourcePath = ns + ".Configuration.index.js"
                 }
             };
