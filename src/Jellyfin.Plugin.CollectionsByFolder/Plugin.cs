18d95c3b7dc0f284df62fcce05d34076706bf112
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-04 14:26:26 +0200

    Update Plugin.cs

--- src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
+++ src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
@@ -1,40 +1,37 @@
-using System;
-using System.Collections.Generic;
-using Jellyfin.Plugin.CollectionsByFolder.Configuration;
-using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
-using MediaBrowser.Model.Serialization; // IXmlSerializer
+using MediaBrowser.Model.Serialization;
+using System.Collections.Generic;
+using MediaBrowser.Model.Drawing;
 
-namespace Jellyfin.Plugin.CollectionsByFolder;
-
-public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+namespace Jellyfin.Plugin.CollectionsByFolder
 {
-    public override string Name => "CollectionsByFolder";
-    public override Guid Id => Guid.Parse("f58f3a40-6a8a-48e8-9b3a-9d7f0b6a3a41");
-
-    public static Plugin Instance { get; private set; } = null!;
-
-    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
-        : base(applicationPaths, xmlSerializer)
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     {
-        Instance = this;
-    }
+        public override string Name => "CollectionsByFolder";
+        public override string Description => "Erstellt Collections anhand von Ordnernamen.";
+        public override Guid Id => Guid.Parse("f58f3a40-6a8a-48e8-9b3a-9d7f0b6a3a41");
 
-    public IEnumerable<PluginPageInfo> GetPages()
-    {
-        return new[]
+        public Plugin(IApplicationPaths appPaths, IXmlSerializer xmlSerializer)
+            : base(appPaths, xmlSerializer)
         {
-            new PluginPageInfo
+        }
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
             {
-                Name = "collectionsbyfolder",
-                EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
-            },
-            new PluginPageInfo
-            {
-                Name = "collectionsbyfolderjs",
-                EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.js"
-            }
-        };
+                new PluginPageInfo
+                {
+                    Name = "collectionsbyfolder",
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.configPage.html"
+                },
+                new PluginPageInfo
+                {
+                    Name = "collectionsbyfolderjs",
+                    EmbeddedResourcePath = GetType().Namespace + ".Configuration.config.js"
+                }
+            };
+        }
     }
 }
