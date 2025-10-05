41a96a8408d4359b33f50f484e1aee65566affa4
Salas79 <144158482+Solas79@users.noreply.github.com>
2025-10-05 20:25:55 +0200

    Update Plugin.cs

--- src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
+++ src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
@@ -1,4 +1,3 @@
-// Datei: src/Jellyfin.Plugin.CollectionsByFolder/Plugin.cs
 using System;
 using System.Collections.Generic;
 using MediaBrowser.Common.Configuration;
@@ -13,28 +12,21 @@ namespace Jellyfin.Plugin.CollectionsByFolder
         public static Plugin Instance { get; private set; } = null!;
         public override string Name => "CollectionsByFolder";
         public override string Description => "Erstellt automatisch Sammlungen nach Ordnernamen.";
-        public override Guid Id => Guid.Parse("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee");
+        public override Guid Id => Guid.Parse("f58f3a40-6a8a-48e8-9b3a-9d7f0b6a3a41");
 
         public Plugin(IApplicationPaths paths, IXmlSerializer xml) : base(paths, xml)
         {
             Instance = this;
         }
 
+        // Nur die HTML-Seite registrieren – keine JS-Seite mehr.
         public IEnumerable<PluginPageInfo> GetPages() => new[]
         {
-            new PluginPageInfo {
-                Name = "collectionsbyfolder",
+            new PluginPageInfo
+            {
+                Name = "collectionsbyfolder", // /web/configurationpage?name=collectionsbyfolder
                 EmbeddedResourcePath = "Jellyfin.Plugin.CollectionsByFolder.configPage.html"
-            },
-            new PluginPageInfo {
-                Name = "collectionsbyfolderjs",
-                EmbeddedResourcePath = "Jellyfin.Plugin.CollectionsByFolder.configPage.js"
-            },
-            new PluginPageInfo {
-                Name = "cbf_ping",
-                EmbeddedResourcePath = "Jellyfin.Plugin.CollectionsByFolder.ping.txt"
             }
         };
-
     }
 }
